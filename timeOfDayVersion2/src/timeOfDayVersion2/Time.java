d897af24eed1551812b3ac3789fc2ca14953c0fc
kobec <kobec@192.168.0.115>
2021-02-13 01:00:55 +0100

    code met van die random comment shit

--- timeOfDayVersion2/src/timeOfDayVersion2/Time.java
+++ timeOfDayVersion2/src/timeOfDayVersion2/Time.java
@@ -1,5 +1,6 @@
 package timeOfDayVersion2;
 
+
 public class Time {
 	
 	private int minutesSinceMidnight;
@@ -12,12 +13,32 @@ public class Time {
 		int hoursNow = getHours();
 		return this.minutesSinceMidnight - hoursNow * 60;
 	}
+	/**
+	 * Stel het aantal uren in
+	 * @throws Het aantal uren ligt tussen 0 en 23
+	 *  | 0 <= newHours && newHours <= 23 
+	 *  
+	 * @post Het nieuwe aantal uren klopt
+	 *  | getHours() == newHours
+	 *  
+	 *  @post Het aantal minuten is niet veranderd
+	 *  | getMinutes() == old(getMinutes())
+	 */
 	
 	public void setHours(int newHours) {		
 		int minutesNow = getMinutes();
 		this.minutesSinceMidnight = newHours * 60 + minutesNow;
 	}
-	
+	/**
+	 * Stelt de minuten in
+	 * 
+	 * @throws Het aantal uren ligt tusse 0 en 59
+	 *  | 0 <= newMinutes && newMinutes <= 59
+	 * @post Het nieuwe aantal minuten klopt
+	 * | getMinutes() == newMinutes
+	 * @post Het aantal uren is niet veranderd
+	 * | getHours() == old(getHours())
+	 */
 	public void setMinutes(int newMinutes) {
 		int hoursNow = getHours();
 		int minutesNow = getMinutes();
